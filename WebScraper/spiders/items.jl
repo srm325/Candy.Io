3bca435e144c2c035ab44801a2ad4383ffa55515
dgilmore33 <dgilmore1997@gmail.com>
2020-10-30 16:08:33 -0400

    web scraper

---
+++ WebScraper/spiders/items.jl
@@ -0,0 +1,216 @@
+{"name": "Canadian $1.00 Loonie Milk Chocolate Coins \u2013 200ct/1kg"}
+{"name": "Bubble Gum Cigarettes \u2013 24 Packs"}
+{"name": "Flipsticks Cherry Taffy Nougat 192 Count"}
+{"name": "Dubble Bubble El Bubble Bubble Gum Cigars- 3 Flavors, Packages (Pack of 36)"}
+{"name": "Dubble Bubble It\u2019s A Boy Bubble Gum Cigars, Packages (Pack of 36)"}
+{"name": "Dubble Bubble It\u2019s A Girl Bubble Gum Cigars Packages (Pack of 72)"}
+{"name": "Wack-o-wax Lips 24 Pack"}
+{"name": "Eclipse Big-e Pack Tray Polar Ice, 4-count"}
+{"name": "Eclipse Big-e Pack Tray Winterfrost, 4-count"}
+{"name": "Eclipse Gum Spearmint Bottle, 120 Count"}
+{"name": "Eclipse Gum, Peppermint, 2.85 Ounce"}
+{"name": "Eclipse Gum, Polar Ice, 2.8 Ounce"}
+{"name": "Eclipse Gum, Polar Ice, 2.9 Ounce"}
+{"name": "Eclipse Gum, Polar Ice, 8 Count"}
+{"name": "Eclipse Gum, Spearmint, 2.8 Ounce"}
+{"name": "Eclipse Gum, Spearmint, 60 Piece Bottle"}
+{"name": "Eclipse Gum, Winterfrost, 2.85 Ounce"}
+{"name": "Eclipse Gum, Winterfrost, 2.9 Ounce"}
+{"name": "Eclipse Gum, Winterfrost, 8.8 Ounce"}
+{"name": "Eclipse Mints Peppermint 8 Packs"}
+{"name": "Eclipse Polar Ice Sugar Free Gum \u2013 12 Pk."}
+{"name": "Eclipse Polar Ice Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Spearmint Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Spearmint Sugarfree Mints,1.2 ounce Boxes (Pack of 8)"}
+{"name": "Eclipse Sugar Free Gum, Polar Ice, 8 Pk"}
+{"name": "Eclipse Sugar Free Gum, Spearmint, 8 Pk"}
+{"name": "Eclipse Sugar Free Gum, Winterfrost, 8 Pk"}
+{"name": "Eclipse Sugar Free Winterfrost Gum, 12-count"}
+{"name": "Eclipse Sugarfree Gum, Peppermint \u2013 18-piece, Pack of 8"}
+{"name": "Eclipse Sugarfree Mints Peppermint, 1.2 ounce Tins (Pack of 16)"}
+{"name": "Eclipse Sugarfree Mints Winterfrost, 1.2 Ounce Tins, 8-count (Pack of 2)"}
+{"name": "Eclipse Winterfrost Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Bige Tray Spearmint 4 Containers"}
+{"name": "Eclipse Mints -eclipse Spearmint Sugarfree Mints (raspberry Flavor Bonus Pack /2 Packs)"}
+{"name": "Eclipse Raspberry Mints -eclipse Spearmint Sugarfree Mints Bonus Pack"}
+{"name": "Eclipse Spearmint Sugarfree Mints (raspberry Flavor Bonus Pack /4 Packs)"}
+{"name": "Canadian $1.00 Loonie Milk Chocolate Coins \u2013 200ct/1kg"}
+{"name": "Bubble Gum Cigarettes \u2013 24 Packs"}
+{"name": "Flipsticks Cherry Taffy Nougat 192 Count"}
+{"name": "Dubble Bubble El Bubble Bubble Gum Cigars- 3 Flavors, Packages (Pack of 36)"}
+{"name": "Dubble Bubble It\u2019s A Boy Bubble Gum Cigars, Packages (Pack of 36)"}
+{"name": "Dubble Bubble It\u2019s A Girl Bubble Gum Cigars Packages (Pack of 72)"}
+{"name": "Wack-o-wax Lips 24 Pack"}
+{"name": "Eclipse Big-e Pack Tray Polar Ice, 4-count"}
+{"name": "Eclipse Big-e Pack Tray Winterfrost, 4-count"}
+{"name": "Eclipse Gum Spearmint Bottle, 120 Count"}
+{"name": "Eclipse Gum, Peppermint, 2.85 Ounce"}
+{"name": "Eclipse Gum, Polar Ice, 2.8 Ounce"}
+{"name": "Eclipse Gum, Polar Ice, 2.9 Ounce"}
+{"name": "Zotz Assorted Sour Fizzing Candies \u2013 2 Lbs"}
+{"name": "Eclipse Gum, Polar Ice, 8 Count"}
+{"name": "Eclipse Gum, Spearmint, 2.8 Ounce"}
+{"name": "Eclipse Gum, Spearmint, 60 Piece Bottle"}
+{"name": "Eclipse Gum, Winterfrost, 2.85 Ounce"}
+{"name": "Eclipse Gum, Winterfrost, 2.9 Ounce"}
+{"name": "Eclipse Gum, Winterfrost, 8.8 Ounce"}
+{"name": "Eclipse Mints Peppermint 8 Packs"}
+{"name": "Eclipse Polar Ice Sugar Free Gum \u2013 12 Pk."}
+{"name": "Eclipse Polar Ice Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Spearmint Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Spearmint Sugarfree Mints,1.2 ounce Boxes (Pack of 8)"}
+{"name": "Eclipse Sugar Free Gum, Polar Ice, 8 Pk"}
+{"name": "Eclipse Sugar Free Gum, Spearmint, 8 Pk"}
+{"name": "Eclipse Sugar Free Gum, Winterfrost, 8 Pk"}
+{"name": "Chinese Guava Hard Candy \u2013 7 Pieces (us Seller)"}
+{"name": "Coffee Candies -coffee Hard Candy Bonus Pack"}
+{"name": "Creme Savers Hard Candy Strawberry & Creme, 6 ounce Bags (Pack of 12)"}
+{"name": "Bon-bons, Strawberry Hard Candy \u2013 5 Pound"}
+{"name": "Richardson After Dinner Mints \u2013 Pastel Mints \u2013 5lb Bag"}
+{"name": "Bon-bons, Assorted, Hard Candy \u2013 5 Pound"}
+{"name": "Chili Filled Hard Candy \u2013 Chilibonchas Mix By Dulces Karla (70 Pieces)"}
+{"name": "Brach???s Butterscotch Hard Candy (1x7lb )"}
+{"name": "Go Lightly Sugar Free Hard Candy Assorted 2.75 Oz Bag, Kosher"}
+{"name": "Go Lightly Sugar Free Hard Candy Assortment, 2.75 Oz Bag, Kosher"}
+{"name": "Go Lightly Sugar Free Hard Candy Cinnamon 2.75 Oz. Bag Kosher (Pack of 12)"}
+{"name": "Go Lightly Sugar Free Hard Candy Lemon, 2.75 Oz, Kosher"}
+{"name": "Eclipse Sugar Free Winterfrost Gum, 12-count"}
+{"name": "Eclipse Sugarfree Gum, Peppermint \u2013 18-piece, Pack of 8"}
+{"name": "Eclipse Sugarfree Mints Peppermint, 1.2 ounce Tins (Pack of 16)"}
+{"name": "Eclipse Sugarfree Mints Winterfrost, 1.2 Ounce Tins, 8-count (Pack of 2)"}
+{"name": "Eclipse Winterfrost Sugarfree Gum, 12-piece Package (Pack of 24)"}
+{"name": "Eclipse Bige Tray Spearmint 4 Containers"}
+{"name": "Eclipse Mints -eclipse Spearmint Sugarfree Mints (raspberry Flavor Bonus Pack /2 Packs)"}
+{"name": "Eclipse Raspberry Mints -eclipse Spearmint Sugarfree Mints Bonus Pack"}
+{"name": "Eclipse Spearmint Sugarfree Mints (raspberry Flavor Bonus Pack /4 Packs)"}
+{"name": "Go Lightly Sugar Free Hard Candy Pomegranate, 2.75 Oz Bag, Kosher"}
+{"name": "Go Lightly Sugar Free Hard Candy Tropical Fruit Assortment, 2.75 Oz Bag, Kosher"}
+{"name": "Golightly Coffee Hard Candy, 5 Lb, Sugar Free, Individually Wrapped (about 600 Pcs)"}
+{"name": "Golightly Sugar Free Lime (5 Lb), Hard Candy, Individually Wrapped"}
+{"name": "Go Naturally Ginger Hard Candy (6\u00d73.5oz )"}
+{"name": "Go Naturally Hard Candy, Blood Orange, 3.5 Ounce (Pack of 6)"}
+{"name": "Go Naturally Hard Candy, Extreme Ginger, 3.5 Ounce (Pack of 6)"}
+{"name": "Go Naturally Organic Gluten Free Hard Candies Iced Mint Mango \u2014 3.5 Oz"}
+{"name": "Go Naturally Organic Gluten Free Hard Candies Pomegranate \u2014 3.5 Oz"}
+{"name": "Go Naturally Organic Hard Candies Apple"}
+{"name": "Go Naturally Organic Hard Candy, Honey Lemon, Bulk/pound, Individually Wrapped"}
+{"name": "Go Naturally Organic Hard Candy, Honey, Bulk/pound, Individually Wrapped"}
+{"name": "Go Naturally Usda Organic Ginger Hard Candy, 3.5 Oz Bag Of Individually Wrapped Candies"}
+{"name": "Go Naturally, Organic, Blood Orange Hard Candy (1-3.5 Oz Bag), Kosher-k"}
+{"name": "Gonaturally Organic Apple Gluten Free Hard Candies, 3.5 ounce Bags (Pack of 6)"}
+{"name": "Gonaturally Organic Assorted Gluten Free Hard Candies, 30 ounce Bag"}
+{"name": "Gonaturally Organic Cherry Gluten Free Hard Candies, 3.5 ounce Bags (Pack of 6)"}
+{"name": "Gonaturally Organic Ginger Gluten Free Hard Candies, 3.5 ounce Bags (Pack of 6)"}
+{"name": "Gonaturally Organic Hard Candies (cherry) (1 X 3.5 Oz)"}
+{"name": "Gonaturally Organic Hard Candies (honey) (1 X 3.5 Oz)"}
+{"name": "Gonaturally Organic Hard Candies (pomegranate) (1 X 3.5 Oz)"}
+{"name": "Gonaturally Organic Honey Gluten Free Hard Candies, 3.5 ounce Bags (Pack of 6)"}
+{"name": "Gonaturally Organic Honey Lemon Gluten Free Hard Candies, 3.5 ounce Bags (Pack of 6)"}
+{"name": "Black Forest Sour Gummy Glo-worms \u2014 4.5 Oz"}
+{"name": "Sour Gummy Glo-worms 4.5lb"}
+{"name": "Blow Pops Sour Apple (Pack of 48)"}
+{"name": "Bubbaloo Gum Sour Cherry (Pack of 60)"}
+{"name": "Bubbaloo Sour Cherry, 60-count (Pack of 6)"}
+{"name": "Bubblicious Burst Sour Cherry Strom Rolls \u2013 7 Piece/ Pack, 12 Packs"}
+{"name": "Bubblicious Bursts Sour Cherry, 7-count (Pack of 12)"}
+{"name": "Bubblicious Sour Apple 5 Piece (Pack of 18)"}
+{"name": "Bubblicious Sour Apple, 18-count"}
+{"name": "Bubblicious Sour Apple, 18-count Packages"}
+{"name": "Bubblicious Sour Apple, 5-count (Pack of 18)"}
+{"name": "Angry Birds Blue Bird Gummies, 5 Ounce Bags, Pack of 12"}
+{"name": "Bubblicious Sour Citrus Bubble Gum, 5-piece Packages (Pack of 36)"}
+{"name": "Bubblicious Sour Citrus, 5-count (Pack of 18)"}
+{"name": "Charms Blow Pops, Sour Apple, 48-count Lollipops (Pack of 3)"}
+{"name": "Charms Candy \u2013 Sour Balls, 12 Oz Tin, 3 Count"}
+{"name": "Charms Naughty Or Nice Sweet \u2018n Sour Pops"}
+{"name": "Charms Pop 48-pops Sweet & Sour"}
+{"name": "Angry Birds Fruit Gummies Combo Case, 3.5oz 12 Pack, 3 Each of Green, Yellow, Red & Blue"}
+{"name": "Angry Birds Fruit Gummies, Yellow Bird, 3.5 Oz., Pack of 12"}
+{"name": "Angry Birds Fruit Gummy Candy, 3.5 Oz, 3 of Each Colorored Box Assortment of 6 Fabulous Flavors, Cherry, Lemon, Blue Raspberry, Apple, Grape and Strawberry"}
+{"name": "Angry Birds Green Bird Gummies, 3.5 Ounce Boxes, Pack of 12"}
+{"name": "Angry Birds Green Bird Gummies, 5 Ounce Bags, Pack of 12"}
+{"name": "Angry Birds Space Black Bird Gummies, 3.5 Ounce Boxes, Pack of 12"}
+{"name": "Angry Birds Space Green Bird Gummies, 5 Ounce Boxes, Pack of 12"}
+{"name": "Angry Birds Space Red Bird Gummies, 3.5 Ounce Boxes, Pack of 12"}
+{"name": "Angry Birds Yellow Bird Gummies, 5 Ounce Bags, Pack of 12"}
+{"name": "Black Forest Gummi Cherries, 4.50 ounce (Pack of 12)"}
+{"name": "Black Forest Gummi Glo-worm, 4.50 ounce (Pack of 12)"}
+{"name": "Black Forest Gummy Bears \u2014 4.5 Oz"}
+{"name": "Black Forest Gummy Bears Hanging Bags, 4.5 ounce Bags (Pack of 12)"}
+{"name": "Charms Sour Balls 12ct"}
+{"name": "Charms Sour Balls, 12 ounce Canisters (Pack of 12)"}
+{"name": "Charms Sour Balls, 12 Oz"}
+{"name": "Charms Sweet & Sour Pops 48ct"}
+{"name": "Charms Sweet and Sour Lollipops 48 Count"}
+{"name": "Charms Sweet and Sour Pops \u2013 48 Lollipops/box"}
+{"name": "Cry Baby Extra Sour Bubble Gum 240ct. Tub"}
+{"name": "Cry Baby Extra Sour Bubble Gumballs, 9-count Tubes (Pack of 24)"}
+{"name": "Cry Baby Sour Mini Drink Bottles 120 Count"}
+{"name": "Cry Baby Tears Extra Sour Candy, Five Flavors, 1.98 ounce Boxes (Pack of 24)"}
+{"name": "Easter Cry Baby Egg Shaped Bubble Gum Filled With Extra Sour Flavor Crystals (3 Pack)"}
+{"name": "Black Forest Gummy Bears, 5-pound Resealable Bags (Pack of 2)"}
+{"name": "Black Forest Gummy Cherries \u2014 4.5 Oz"}
+{"name": "Black Forest Gummy Mellos Peach Candy \u2013 Pegboard, 4.25 Ounce Bag \u2014 12 Per Case."}
+{"name": "Black Forest Gummy Worms \u2014 4.5 Oz"}
+{"name": "Black Forest Gummy Worms, 5-pound Resealable Bags (Pack of 2)"}
+{"name": "Black Forest Juicy Oozer Cherry Gummy Sharks, 4.25 Ounce Bag \u2014 12 Per Case."}
+{"name": "Black Forest Juicy Oozers Gummy Sharks Cherry \u2014 4.25 Oz"}
+{"name": "Black Forest Sour Gummy Glo-worms \u2014 4.5 Oz"}
+{"name": "Black Forest Swirly Gummy Bears \u2014 4.5 Oz"}
+{"name": "Black Forest Swirly Gummy Bears 5 Oz. (Pack of 3)"}
+{"name": "Black Forest(r) Gummy Bears, 48 Oz. Bag"}
+{"name": "Disney Parks Goofy Candy Co. Sour Taffy 1.25 Pound Box New"}
+{"name": "Disney World Parks Goofy Candy Co. Assorted Flavor Sour Balls Family Size 7 Oz. Bag Sealed \u2013 New"}
+{"name": "Disney World Parks Goofy Candy Co. Sour Gummi Worms Family Size 6 Oz. Bag Sealed \u2013 New"}
+{"name": "Sweet Tarts Twist Roll, 3lb"}
+{"name": "Dorval Quatro Multi Flavored Sour Power Candy Belts 150 Individually Wrapped Pieces"}
+{"name": "Dorval Sour Power Belts Blue Raspberry Candy Belts"}
+{"name": "Dorval Sour Power Sortz Candy Straws Twenty Four Count Box"}
+{"name": "Sour Belts: Strawberry 6lb Bag"}
+{"name": "Black Forest, Gummy Worms, 48oz Bag (Pack of 2)"}
+{"name": "Black Forest-gummy Bears, 6lb. Bag"}
+{"name": "Gummy Glow Worm Candy"}
+{"name": "Sour Gummy Glo-worms 4.5lb"}
+{"name": "Bright Pink Bubble Gum Flavored Gummy Bears 5 Pound Bulk Bag"}
+{"name": "Disney World Parks Goofy Candy Co. Sour Gummi Worms Family Size 6 Oz. Bag Sealed \u2013 New"}
+{"name": "Family Favorites Fun Size Skittles, Starburst Life & Savers Gummies"}
+{"name": "Fruit Candy /fruit Gummy -mixed Fruit Candy Assorted Gummy Bonus Pack (3 Packs)"}
+{"name": "Fruit Gummy /fruit Candy -mixed Fruit Candy Assorted Gummy Bonus Pack"}
+{"name": "Brach\u2019s Wild N Fruity Gummi Bears, 8-pound (1x8lb )"}
+{"name": "Green Apple Kiwi Gummy Bears 5 Pound Bulk Bag"}
+{"name": "Hershey\u2019s All Chocolate Pieces, 90 Ounce"}
+{"name": "Mars Halloween Assorted Chocolate Trick-or-treat Candy Mix 67.85oz."}
+{"name": "Tootsie Roll Break Bites, Chocolate Candy, 17oz Bowl"}
+{"name": "Andes Chocolate Mint Thins, 120ct"}
+{"name": "Andes Creme De Menthe Milk Chocolate, 12 Pack"}
+{"name": "Bark Thins Dark Chocolate Amond With Sea Salt \u2013 4.7 Oz"}
+{"name": "Bark Thins Dark Chocolate Mint \u2013 4.7 Oz"}
+{"name": "Bark Thins Snacking Chocolate Dark Chocolate Pretzel With Sea Salt \u2014 4.7 Oz"}
+{"name": "Bonomos Turkish Taffy \u2013 Chocolate 24ct."}
+{"name": "Boost Nutrition Bar, Chocolate 8 Ct (Pack of 6)"}
+{"name": "Bounty Chocolate Bars, 12-count"}
+{"name": "Bounty Chocolate Bars, 6-count"}
+{"name": "Burst\u2019s Chocolates \u2013 9 Piece Assorted Truffle Box Of Chocolates"}
+{"name": "Burst\u2019s Chocolates \u2013 Best Chocolate Assortment: Creams & Chews"}
+{"name": "Burst\u2019s Chocolates \u2013 Best Chocolate Assortment: Nuts & Chews"}
+{"name": "Butterfinger Chocolate Bar 2.1 Oz"}
+{"name": "Butterfinger Chocolate Bar 2.1 Oz (Pack of 36)"}
+{"name": "Butterfinger Medallion Chocolates, Christmas, 1.2 ounce Packages (Pack of 36)"}
+{"name": "Butterfinger Solid Milk Chocolate Bunny, 5.5 Ounce (Pack of 12)"}
+{"name": "Canadian $1.00 Loonie Milk Chocolate Coins \u2013 200ct/1kg"}
+{"name": "Candyland Chocolate Editions Of Hasbro Games Candy, 5.4 Ounce (Pack of 6)"}
+{"name": "Carlos V Milk Chocolate Style Bar"}
+{"name": "Carousel Candies Caramel and Chocolate Artisan Pretzel Rods 2 Lb."}
+{"name": "Cella\u2019s Chocolate Covered Cherries \u2013 Box Of 72"}
+{"name": "Cella\u2019s Dark Chocolate Covered Cherries, 72-count Box"}
+{"name": "Cellas Dark Chocolate Covered Cherry 72 Count Box"}
+{"name": "Cella\u2019s Milk Chocolate 12 Count(Pack of 2 Boxes) 24 Total"}
+{"name": "Cella\u2019s Milk Chocolate Covered Cherries, 72-count"}
+{"name": "Cella\u2019s Milk Chocolate Covered Cherries, 72-count Box"}
+{"name": "Cella\u2019s Milk Chocolate Covered Cherries, 72ct"}
+{"name": "Charleston Chew (Pack of 24) Chocolate"}
+{"name": "Charleston Mini Chews Chewy Flavored Nougat, Chocolate, 1.875 ounce Bars (Pack of 24)"}
+{"name": "Chocomilk Chocolate Milk Flavoring 14.1 Oz."}
+{"name": "Chocolate By Numbers 38% Dreamy Chocolate, 1 Ounce Mini Boxes (Pack of 18)"}
+{"name": "Chocolate By Numbers 55% Premium Dark Chocolate, 1 Ounce Mini Boxes (Pack of 18)"}
+{"name": "Chocolate By Numbers 58% Premium Dark Chocolate, 1.25 ounce Mini Boxes (Pack of 18)"}
